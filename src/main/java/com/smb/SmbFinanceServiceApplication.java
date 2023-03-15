98bb7b7f5d34e242e9eebbb0a6ca464787d5eb84
nellorukiran <nellorukiran@gmail.com>
2023-03-15 11:59:40 +0530

    First commit

---
+++ src/main/java/com/smb/SmbFinanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmbFinanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmbFinanceServiceApplication.class, args);
+	}
+
+}
